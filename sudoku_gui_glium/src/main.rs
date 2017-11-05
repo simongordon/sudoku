0d09865f5edfe6b2e411836ea0f9db94482dc634
Simon Gordon <gordonsimon@hotmail.com>
2017-11-05 18:31:34 +1000

    Show characters on every square

--- sudoku_gui_glium/src/main.rs
+++ sudoku_gui_glium/src/main.rs
@@ -15,13 +15,15 @@ use piston::input::*;
 use piston::event_loop::EventLoop;
 use piston::window::WindowSettings;
 use graphics::draw_state::Blend;
+use std::path::Path;
+
+use graphics::character::*;
 
 const SQUARE_WIDTH: f64 = 50.0;
 
 fn main() {
     let mut game_board = Board::default();
     let num_groups = game_board.side_length;
-
 
     let opengl = OpenGL::V3_2;
     let (w, h) = (640, 480);
@@ -31,6 +33,17 @@ fn main() {
 
     let mut g2d = Glium2d::new(opengl, window);
 
+    let mut glyph_cache = GlyphCache::new(
+        Path::new("assets/FiraSans-Regular.ttf"),
+        window.clone(),
+        TextureSettings::new()
+    ).unwrap();
+
+    let black = [0.0, 0.0, 0.0, 1.0];
+    let white = [1.0, 1.0, 1.0, 1.0];
+    let grey = [0.8, 0.8, 0.8, 1.0];
+    let red = [1.0, 0.0, 0.0, 1.0];
+
     window.set_lazy(true);
     while let Some(e) = window.next() {
         if let Some(args) = e.render_args() {
@@ -38,28 +51,28 @@ fn main() {
 
             let mut target = window.draw();
             g2d.draw(&mut target, args.viewport(), |c, g| {
+                clear(grey, g); // Grey background
+                let text_image = Image::new_color(red);
                 for col_num in 0..num_groups {
                     let x: f64 = (col_num as f64) * SQUARE_WIDTH;
                     for row_num in 0..num_groups {
 
                         let y: f64 = (row_num as f64) * SQUARE_WIDTH;
-                        
-                        let black = [0.0, 0.0, 0.0, 1.0];
-                        let white = [1.0, 1.0, 1.0, 1.0];
 
                         let full_width = SQUARE_WIDTH;
                         let smaller_width = SQUARE_WIDTH * 0.9;
 
-                        Rectangle::new(black)
-                            .draw([x, y, full_width, full_width], &c.draw_state, c.transform, g);
+                        Rectangle::new(black).draw([x, y, full_width, full_width], &c.draw_state, c.transform, g);
+                        Rectangle::new(white).draw([x, y, smaller_width, smaller_width], &c.draw_state, c.transform, g);
 
-                        Rectangle::new(white)
-                            .draw([x, y, smaller_width, smaller_width], &c.draw_state, c.transform, g);
+                        let character = glyph_cache.character(34, 'a').unwrap();
+                        text_image.draw(character.texture,
+                                        &c.draw_state,
+                                        c.transform.trans(x, y),
+                                        g);
                     }
                 }
 
-                // Grey background
-                clear([0.8, 0.8, 0.8, 1.0], g);
 
             });
 
